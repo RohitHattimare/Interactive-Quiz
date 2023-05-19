ff51c2ec83aa9cca98898a58d7227a71a1272990
xpknight <xpknight@gmail.com>
2023-05-19 16:11:53 +0530

    Score Calculation added

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const ans = ['a','a','b','a'];
+const correctAns = ["A","A","B","A"];
+
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAns = [ form.q1.value, form.q2.value, form.q3.value,form.q4.value];
+
+    userAns.forEach((ans,index)=>{
+        if(ans ===correctAns[index])
+        {
+            score += 25;
+        }
+    });
+
+    //Show Result
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
+});
